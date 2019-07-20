6308f324ff83430a54c8a8889d87f4bb47620777
a278779191 <18001164010@163.com>
2019-07-20 10:53:54 +0800

    exchenge pom.xml

--- src/main/java/life/zhiyuanhome/oranger/mapper/UserMapper.java
+++ src/main/java/life/zhiyuanhome/oranger/mapper/UserMapper.java
@@ -2,14 +2,16 @@ package life.zhiyuanhome.oranger.mapper;
 
 import life.zhiyuanhome.oranger.model.User;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author yzy
  * @create 2019-07-18 21:26
  */
+@Mapper
 public interface UserMapper {
 
-    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmt_create},#{gmt_modified}) ")
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{Token},#{gmtCreate},#{gmtModified}) ")
     void insert(User user);
 
 
